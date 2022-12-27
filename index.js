54e827d9b6169076dffb26f572c9f1ebff6a1200
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-27 12:26:14 +0100

    ok

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1> Hello everyone</h1>, document.getElementById("root"));
+function Navbar() {
+  return (
+    <div>
+      <h1>Thisi is a main content</h1>
+      <p>Money is important in life it also makes you proud</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<h1>Hey brother</h1>, document.querySelector("#cool"));
