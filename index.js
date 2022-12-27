b57a54f92f98eaaeb5ddddfadd5021a086be38fa
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-27 21:39:33 +0100

    learning jsx

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 function Navbar() {
   return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function mainContent() {
+  return (
     <div>
-      <h1>Thisi is a main content</h1>
-      <p>Money is important in life it also makes you proud</p>
+      <h1>Hi, there i am learning react</h1>
+      <p>React is so cool and i am learning it to land a job</p>
     </div>
   );
 }
 
-ReactDOM.render(<h1>Hey brother</h1>, document.querySelector("#cool"));
+ReactDOM.render(
+  <div>
+    <Navbar />
+    <mainContent />
+  </div>,
+  document.querySelector("#cool")
+);
