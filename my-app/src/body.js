fa5e7af2c643d4c6f9cbc57ce415d02b476d540b
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-04 22:51:10 +0100

    ok

--- my-app/src/body.js
+++ my-app/src/body.js
@@ -5,6 +5,14 @@ export default function Card() {
   return (
     <main className="card">
       <img src={FirstImg} alt="portrait" className="pro1" />
+      <div className="card-stats">
+        <p>💖</p>
+        <span>5.0</span>
+        <span>(6) * </span>
+        <span>USA</span>
+      </div>
+      <p>Life lessons with richie Zafares</p>
+      <p>From $136 / person</p>
     </main>
   );
 }
