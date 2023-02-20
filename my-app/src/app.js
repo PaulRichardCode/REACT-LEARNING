1faf2f9948cd3e4e681ce115255af168c27ad5dc
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-20 12:46:19 +0100

    kk

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/header/header";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Header />
--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import css from "./header.module.css";
+import Logo from "../../assets/logo.png";
+import { CgShoppingBag } from "";
 
-function header() {
+export default function header() {
   return (
-    <div>
-      <h1>Welcome to my App</h1>
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="logo" />
+        <span>amazon</span>
+      </div>
+
+      <div className={css.rightside}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+
+        <input type="text" id="search" className={css.search} />
+        <CgShoppingBag className={css.cart} />
+      </div>
     </div>
   );
 }
