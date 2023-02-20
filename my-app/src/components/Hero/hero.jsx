5bd4a2e2891c9f3adaf0fd203ce739458ecdc828
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-20 13:25:05 +0100

    yeah

---
+++ my-app/src/components/Hero/hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import css from "./hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={css.container}>
+      {/* Left hand side */}
+
+      <div className={css.h_sides}>
+        <span>skin protection cream</span>
+
+        <div className={css.text2}>
+          <span>Trendy Coll Lorem ipsum dolor sit amet, consectection</span>
+          <span>etur adipisicing elit. Eos.</span>
+        </div>
+      </div>
+
+      {/* Middle side here image */}
+
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt="" width={600} />
+        <div className={css.cart2}> </div>
+      </div>
+    </div>
+  );
+}
