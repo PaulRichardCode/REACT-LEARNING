9d7f896bb68128b4744d7375da87a269e813d232
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-27 04:15:03 +0100

    working hard

--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import { SliderProducts } from "../../data/products";
 
 export default function scroller() {
-  const products = SliderProducts.map((produce) => {
-    produce.name;
-  });
-
-  return <div className="py-32">{products}</div>;
+  return (
+    <div className="py-32 flex items-center justify-center">
+      <div className="flex container w-72 h-52 bg-white rounded-3xl p-8 border-black">
+        <div>
+          <h3 className="text-2xl font-bold">SKIN</h3>
+          <span className="block leading-3">mommi</span>
+          <h3>$25</h3>
+          <p>Indian hemp</p>
+        </div>
+        <div>
+          <img src="#" alt="bro" />
+        </div>
+      </div>
+    </div>
+  );
 }
