61ee3720f98bb1f70f7cc98b4b7e013e69cb4ecd
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-27 04:32:25 +0100

    we are done

--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -4,12 +4,14 @@ import { SliderProducts } from "../../data/products";
 export default function scroller() {
   return (
     <div className="py-32 flex items-center justify-center">
-      <div className="flex container w-72 h-52 bg-white rounded-3xl p-8 border-black">
-        <div>
-          <h3 className="text-2xl font-bold">SKIN</h3>
-          <span className="block leading-3">mommi</span>
-          <h3>$25</h3>
-          <p>Indian hemp</p>
+      <div className="flex container w-72 h-56 bg-white rounded-3xl  border-black">
+        <div className="p-5">
+          <h3 className="text-3xl font-bold">SKIN</h3>
+          <span className="block">mommi</span>
+          <h3 className="py-6 font-extrabold text-4xl">25$</h3>
+          <p className="border-2 border-black/50 p-2 text-xs rounded-2xl">
+            Indian hemp
+          </p>
         </div>
         <div>
           <img src="#" alt="bro" />
