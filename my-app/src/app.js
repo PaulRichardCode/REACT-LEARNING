a63b7332f610d0a48f3ff41189e30eeef8f28410
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-02-28 16:25:00 +0100

    lets go

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -2,13 +2,26 @@ import React from "react";
 import Header from "./components/header/header";
 import Scroll from "./components/scroll/scroll";
 import Hero from "./components/Hero/hero";
+import { ProductsData } from "./data/products";
 
 export default function App() {
+  const Data = ProductsData.map((items) => {
+    return (
+      <Scroll
+        name={items.name}
+        img={items.img}
+        detail={items.detail}
+        price={items.price}
+        type={items.type}
+      />
+    );
+  });
+
   return (
     <div className="bg-yellow-300">
       <Header />
       <Hero />
-      <Scroll />
+      <section className="flex overflow-hidden">{Data}</section>
     </div>
   );
 }
--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import img from "../../assets/img1.png";
 
-export default function scroller() {
+export default function scroller(props) {
   return (
     <div className="py-32 flex items-center justify-center">
-      <div className="flex container w-72 h-56 bg-white rounded-3xl justify-around border-black cursor-pointer">
+      <div className="flex container w-72 h-56 bg-yellow-50 rounded-3xl justify-around border-black cursor-pointer">
         <div className="p-5">
-          <h3 className="text-3xl font-bold">SKIN</h3>
-          <span className="block">mommi</span>
-          <h3 className="py-6 font-extrabold text-4xl">25$</h3>
+          <h3 className="text-3xl font-bold">{props.name}</h3>
+          <span className="block">{props.detail}</span>
+          <h3 className="py-6 font-extrabold text-4xl">{props.price}</h3>
           <p className="border-2 border-black/50 p-2 text-xs rounded-2xl">
-            Indian hemp
+            {props.type}
           </p>
         </div>
         <div>
-          <img src={img} alt="bro" className="-rotate-12 h-60" />
+          <img src={props.img} alt="bro" className="-rotate-12 h-60" />
         </div>
       </div>
     </div>
