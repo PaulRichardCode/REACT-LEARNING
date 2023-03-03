2abddc3746dd59992301a93a18a98cfd841d1075
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-03 09:23:24 +0100

    i finally got it

--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -3,31 +3,33 @@ import img from "../../assets/img1.png";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { ProductsData } from "../../data/products";
+import { SliderProducts } from "../../data/products";
 
-export default function scroller() {
-  <section id="Testimonials">
-    <h5>Review from clients</h5>
-    <h2>Testimonials</h2>
-    <swiper className="container">
-      {ProductsData.map(({ name, img, detail, type, price }, index) => {
-        return (
-          <SwiperSlide key={index}>
-            <div className="client">
-              <img src={img} alt="img" />
+export default function scroller(props) {
+  return (
+    <div>
+      <swiper>
+        <SwiperSlide>
+          <div className="flex container w-72 h-56 bg-yellow-50 rounded-3xl justify-around border-black cursor-pointer">
+            <div className="p-5">
+              <h3 className="text-3xl font-bold">{props.name}</h3>
+              <span className="block">{props.detail}</span>
+              <h3 className="py-6 font-extrabold text-4xl">{props.price}</h3>
+              <p className="border-2 border-black/50 p-2 text-xs rounded-2xl">
+                {props.type}
+              </p>
             </div>
-            <h3 className="text-3xl font-bold">{props.name}</h3>
-            <span className="block">{props.detail}</span>
-            <h3 className="py-6 font-extrabold text-4xl">{props.price}</h3>
-            <p className="border-2 border-black/50 p-2 text-xs rounded-2xl">
-              {props.type}
-            </p>
-          </SwiperSlide>
-        );
-      })}
-    </swiper>
-  </section>;
+            <div>
+              <img src={props.img} alt="bro" className="-rotate-12 h-60" />
+            </div>
+          </div>
+        </SwiperSlide>
+        ;
+      </swiper>
+    </div>
+  );
 }
 
 {
