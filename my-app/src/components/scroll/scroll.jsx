a6253066dd9ff34f500b687f18b3721547e78110
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-03 08:51:21 +0100

    learning coding

--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -1,48 +1,20 @@
 import React from "react";
 import img from "../../assets/img1.png";
+import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import { ProductsData } from "../../data/products";
 
-export default function scroller() {
-  return (
-    <div className="s-container">
-      <Swiper
-        // install Swiper modules
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}>
-        {ProductsData.map((slide) => {
-          <SwiperSlide>
-            <div className="py-32 flex items-center justify-center">
-              <div className="flex container w-72 h-56 bg-yellow-50 rounded-3xl justify-around border-black cursor-pointer">
-                <div className="p-5">
-                  <h3 className="text-3xl font-bold">baddest</h3>
-                  <span className="block">{slide.detail}</span>
-                  <h3 className="py-6 font-extrabold text-4xl">
-                    {slide.price}
-                  </h3>
-                  <p className="border-2 border-black/50 p-2 text-xs rounded-2xl">
-                    {slide.type}
-                  </p>
-                </div>
+export default function scroller(props) {
+  <section id="Testimonials">
+    <h5>Review from clients</h5>
+    <h2>Testimonials</h2>
 
-                <img src={slide.img} alt="bro" className="-rotate-12 h-60" />
-              </div>
-            </div>
-          </SwiperSlide>;
-        })}
-      </Swiper>
-      );
-    </div>
-  );
+    <swiper>
+      return(
+      <h1>pogers</h1>)
+    </swiper>
+  </section>;
 }
 
 {
