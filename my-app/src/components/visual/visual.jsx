2f78579e94f51d2ae9723574df555a773e6078ab
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-05 08:25:30 +0100

    b3 low

--- my-app/src/components/visual/visual.jsx
+++ my-app/src/components/visual/visual.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Shade from "../../assets/shade.png";
 
 const visual = () => {
   return (
-    <div>
-      <h1>boss</h1>
-      <h2>sup</h2>
+    <div className="container mx-auto w-3/4 text-center flex justify-around">
+      <div className="w-1/3 bg-red-400 text-left ">
+        <span className="block text-3xl font-bold">VIRTUAL TRY-ON</span>
+        <span className="block text-2xl w-60 py-10 uppercase">
+          Never Buy The Wrong Shade Again
+        </span>
+        <span className="block text-2xl font-bold"> Try NOW!</span>
+        <img src={Shade} alt="Shade" />
+      </div>
+      <div className="w-1/2"></div>
     </div>
   );
 };
