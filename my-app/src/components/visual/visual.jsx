d911c4f3269e939f36e0b657f36b8f93fc019975
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-05 08:28:41 +0100

    okay

--- my-app/src/components/visual/visual.jsx
+++ my-app/src/components/visual/visual.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Shade from "../../assets/shade.png";
+import Before from "../../assets/before.png";
+import After from "../../assets/after.png";
 
 const visual = () => {
   return (
     <div className="container mx-auto w-3/4 text-center flex justify-around">
-      <div className="w-1/3 bg-red-400 text-left ">
+      <div className="w-1/3 mx-auto  text-left ">
         <span className="block text-3xl font-bold">VIRTUAL TRY-ON</span>
         <span className="block text-2xl w-60 py-10 uppercase">
           Never Buy The Wrong Shade Again
         </span>
         <span className="block text-2xl font-bold"> Try NOW!</span>
-        <img src={Shade} alt="Shade" />
+        <img src={Shade} alt="Shade" className="w-32" />
       </div>
       <div className="w-1/2"></div>
     </div>
