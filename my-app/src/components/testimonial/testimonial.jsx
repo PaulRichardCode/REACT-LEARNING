78bf93dbbd50e32e4a39df0f5dc72a1a18595330
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-18 12:20:51 +0100

    j

--- my-app/src/components/testimonial/testimonial.jsx
+++ my-app/src/components/testimonial/testimonial.jsx
@@ -5,18 +5,22 @@ import Hero from "../../assets/testimonialHero.png";
 const Testimonial = () => {
   return (
     <div className="flex justify-between items-end w-11/12 mx-auto py-10">
-      <div className="basis-2/6">
-        <h1>Top Rated</h1>
-        <p>
-          Seedily say has suitable disposal and boy. Exercise joy man children
-          rejoiced.
-        </p>
+      <div className="basis-2/6 ">
+        <div className="w-3/4  uppercase">
+          <h1 className="text-2xl py-5 font-bold">Top Rated</h1>
+          <p className="text-xs font-medium">
+            Seedily say has suitable disposal and boy. Exercise joy man children
+            rejoiced.
+          </p>
+        </div>
       </div>
 
       <img src={Hero} alt="hero" className="basis-2/6 w-96" />
-      <div className="basis-2/6 w-1/5">
-        <h2>100K</h2>
-        <p>Happy Customers with us</p>
+      <div className="basis-2/6 ">
+        <div className="text-left">
+          <h2>100K</h2>
+          <p>Happy Customers with us</p>
+        </div>
       </div>
     </div>
   );
