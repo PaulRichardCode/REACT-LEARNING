caf28c119456c2f1b46ce1e5e5cefd48d2dd1e79
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-18 12:30:24 +0100

    k

--- my-app/src/components/testimonial/testimonial.jsx
+++ my-app/src/components/testimonial/testimonial.jsx
@@ -7,7 +7,7 @@ const Testimonial = () => {
     <div className="flex justify-between items-end w-11/12 mx-auto py-10">
       <div className="basis-2/6 ">
         <div className="w-3/4  uppercase">
-          <h1 className="text-2xl py-5 font-bold">Top Rated</h1>
+          <h1 className="text-3xl py-5 font-bold">Top Rated</h1>
           <p className="text-xs font-medium">
             Seedily say has suitable disposal and boy. Exercise joy man children
             rejoiced.
@@ -15,11 +15,11 @@ const Testimonial = () => {
         </div>
       </div>
 
-      <img src={Hero} alt="hero" className="basis-2/6 w-96" />
+      <img src={Hero} alt="hero" className="basis-1/6 w-72" />
       <div className="basis-2/6 ">
-        <div className="text-left">
-          <h2>100K</h2>
-          <p>Happy Customers with us</p>
+        <div className="text-right uppercase">
+          <h2 className="text-3xl pb-8 font-bold">100K</h2>
+          <p className="text-xs font-medium">Happy Customers with us</p>
         </div>
       </div>
     </div>
